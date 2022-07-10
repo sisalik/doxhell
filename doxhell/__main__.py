af5197a9ada061ab5864d29d3fd2bb605f950031
Siim Lepik <siim.lepik@gmail.com>
2022-07-10 01:10:58 +0100

    Use click instead of argparse

--- doxhell/__main__.py
+++ doxhell/__main__.py
@@ -1,35 +1,12 @@
-import argparse
 import sys
+from typing import Tuple
 
+import click
 from loguru import logger
 
 import doxhell.loaders
 import doxhell.outputs
 import doxhell.reviewers
-
-
-def _parse_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description="Doxhell")
-    parser.add_argument(
-        "--docs-dir",
-        default=".",
-        help="The directory containing the Doxhell documentation. Defaults to the "
-        "current directory.",
-    )
-    parser.add_argument(
-        "--test-dir",
-        default=".",
-        help="The directory containing the automated tests. Defaults to the current "
-        "directory.",
-    )
-    parser.add_argument(
-        "-v",
-        "--verbose",
-        action="count",
-        default=0,
-        help="Increase verbosity of logging output.",
-    )
-    return parser.parse_args()
 
 
 def _setup_logging(verbosity: int) -> None:
@@ -44,14 +21,46 @@ def _setup_logging(verbosity: int) -> None:
         raise ValueError(f"Invalid verbosity level: {verbosity}")
 
 
-def main(args: argparse.Namespace) -> int:
-    """Run the Doxhell review tool."""
-    _setup_logging(args.verbose)
+@click.group()
+def cli():
+    """Command line interface entry point."""
+
+
+@cli.command()
+@click.option(
+    "-v",
+    "--verbose",
+    "verbosity",
+    default=0,
+    count=True,
+    help="Increase verbosity of logging output. Can be used multiple times, e.g. -vv.",
+)
+@click.option(
+    "-t",
+    "--test-dir",
+    "test_dirs",
+    default=(".",),
+    multiple=True,
+    help="The directory containing the automated tests.",
+)
+@click.option(
+    "-d",
+    "--docs-dir",
+    "docs_dirs",
+    default=(".",),
+    multiple=True,
+    help="The directory containing the documentation files.",
+)
+def review(
+    verbosity: int, test_dirs: Tuple[str, ...], docs_dirs: Tuple[str, ...]
+) -> None:
+    """Validate requirements and tests; check coverage."""
+    _setup_logging(verbosity)
     # Load all requirements and tests and convert to lists since we need to iterate
     # over them multiple times
-    requirements = list(doxhell.loaders.load_requirements(args.docs_dir))
-    manual_tests = list(doxhell.loaders.load_manual_tests(args.docs_dir))
-    automated_tests = list(doxhell.loaders.load_automated_tests(args.test_dir))
+    requirements = list(doxhell.loaders.load_requirements(docs_dirs[0]))
+    manual_tests = list(doxhell.loaders.load_manual_tests(docs_dirs[0]))
+    automated_tests = list(doxhell.loaders.load_automated_tests(test_dirs[0]))
     all_tests = automated_tests + manual_tests
 
     doxhell.reviewers.map_coverage(requirements, all_tests)
@@ -61,10 +70,11 @@ def main(args: argparse.Namespace) -> int:
 
     doxhell.outputs.print_coverage_summary(requirements)
     doxhell.outputs.print_problems(problems)
-    # If there are any problems, return a non-zero exit code
-    return len(problems)
+    # If there were any problems, return a non-zero exit code
+    sys.exit(len(problems))
 
 
 if __name__ == "__main__":
+    cli()
     # Run the main function with command line args and exit with the returned exit code
-    sys.exit(main(_parse_args()))
+    # sys.exit(main(_parse_args()))
